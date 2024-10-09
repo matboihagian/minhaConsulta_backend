f5af1eb63a7074574b30dd803455d5a4e3717439
matboihagian <mboihagian@gmail.com>
2024-10-09 09:38:48 -0300

    aula09/10

--- db/database.js
+++ db/database.js
@@ -26,6 +26,18 @@ db.serialize(() => {
     userId INTEGER,
     FOREIGN KEY (userId) REFERENCES users(id)
   )`);
+  
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS consultations (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId INTEGER NOT NULL,
+      date TEXT NOT NULL,
+      doctor TEXT NOT NULL,
+      specialty TEXT NOT NULL,
+      status TEXT NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    )`);
+  });
 });
 
 module.exports = db;
